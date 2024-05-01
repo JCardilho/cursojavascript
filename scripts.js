f8f8771a91363f6fe19492b2a84ec95945981ca5
Jose Luiz <115733809+JCardilho@users.noreply.github.com>
2024-05-01 11:29:48 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,6 +2,14 @@ const url = "htttps://jsonplaceholder.typicode.co/posts"
 const loadingElement = document.querySelector("#loading")
 const postsContainer = document.querySelector("posts-container");
 
+const postPage = document.querySelector("#post")
+const postContainer =document.querySelector("#post-container");
+const commentsContainer = document.querySelector("#coments-container");
+
+
+// Get id from url
+const urlSearchParams = new URLSearchParams(window.location.search);
+const postId = urlSearchParams.get("id")
 
 //Get all posts
 async function getAllPosts(){
@@ -28,4 +36,21 @@ async function getAllPosts(){
         postsContainer.appendChild(div);
     })
 }
-getAllPosts();
+//Get individual post
+async function getPost(id){
+    const [responsePost, responseComents] = await Promise.all([
+        fetch('${url}/${id}'),
+        fetch('${url}/${id}/coments')
+    ])
+
+    const dataPost = await responsePost.json()
+    const dataComents = await responseComents.json()
+    loadingElement.classList.add("hide");
+    postPage.classList.remove("hide");
+}
+
+if(!postId){
+    getAllPosts();
+}else {
+    getPost(postId);
+}
